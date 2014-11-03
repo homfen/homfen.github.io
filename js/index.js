3e09f892b0c18bcb1d29802230450fedb9743302
homfen <homfen@homfens-MacBook-Pro.local>
2014-11-04 02:36:23 +0800

    edit

--- js/index.js
+++ js/index.js
@@ -147,8 +147,11 @@ $(function(){
         $(str).appendTo($('.xiami'));
 
     });*/
-    var list = Math.floor(Math.random()*2)+1;
-    $(".xiamicontent").html('<embed src="http://www.xiami.com/widget/5850808_'+songList[list]+',_180_140_FF8719_000000_1/multiPlayer.swf" type="application/x-shockwave-flash" width="180" height="140" wmode="opaque">');
+    var listIndex = Math.floor(Math.random()*2)+1;
+    var songsArr = songList[listIndex].split(',');
+    songsArr = songsArr.sort(randomSort);
+    var songStr = songsArr.join(',');
+    $(".xiamicontent").html('<embed src="http://www.xiami.com/widget/5850808_'+songStr+',_180_140_FF8719_000000_1/multiPlayer.swf" type="application/x-shockwave-flash" width="180" height="140" wmode="opaque">');
 	
 });
 function setRotate(node){
